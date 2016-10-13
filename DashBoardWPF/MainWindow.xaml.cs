e395dcd6905314899540d2b191be3de3dfa7fb3c
Nguyen Cao <nnp.cao@gmail.com>
2016-10-13 17:07:34 +0200

    Display schedules of rer a, 157, 160, 378.

--- DashBoardWPF/MainWindow.xaml.cs
+++ DashBoardWPF/MainWindow.xaml.cs
@@ -43,24 +43,50 @@ namespace DashBoardWPF
         private void GetData(object sender, EventArgs e)
         {
             HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://api-ratp.pierre-grimaud.fr/v2/rers/A/stations/22?destination=2");
+            client.BaseAddress = new Uri("http://api-ratp.pierre-grimaud.fr/v2/");
 
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
-            HttpResponseMessage response = client.GetAsync("22?destination=2").Result;
+            HttpResponseMessage respRERA = client.GetAsync("rers/A/stations/22?destination=2").Result;
+            HttpResponseMessage respBus157 = client.GetAsync("bus/157/stations/ville-faidherbe-rer?destination=pont+de+neuilly").Result;
+            HttpResponseMessage respBus160 = client.GetAsync("bus/160/stations/nanterre-ville+rer?destination=pont+de+sevres").Result;
+            HttpResponseMessage respBus378g = client.GetAsync("bus/378/stations/nanterre-ville-rer?destination=les+courtilles+metro").Result;
+            HttpResponseMessage respBus378j = client.GetAsync("bus/378/stations/jules+quentin?destination=les+courtilles+metro").Result;
 
-            if (response.IsSuccessStatusCode)
+            if (respRERA.IsSuccessStatusCode)
             {
-                var jsonString = response.Content.ReadAsStringAsync();
-                jsonString.Wait();
-                dynamic jsonResponse = JsonConvert.DeserializeObject(jsonString.Result);
+                var jsonStringRERA = respRERA.Content.ReadAsStringAsync();
+                var jsonStringBus157 = respBus157.Content.ReadAsStringAsync();
+                var jsonStringBus160 = respBus160.Content.ReadAsStringAsync();
+                var jsonStringBus378g = respBus378g.Content.ReadAsStringAsync();
+                var jsonStringBus378j = respBus378j.Content.ReadAsStringAsync();
 
-                rer_list.ItemsSource = jsonResponse.response.schedules;
+                jsonStringRERA.Wait();
+                jsonStringBus157.Wait();
+                jsonStringBus160.Wait();
+                jsonStringBus378g.Wait();
+                jsonStringBus378j.Wait();
+
+                dynamic jsonResponseRERA = JsonConvert.DeserializeObject(jsonStringRERA.Result);
+                dynamic jsonResponseBus157 = JsonConvert.DeserializeObject(jsonStringBus157.Result);
+                dynamic jsonResponseBus160 = JsonConvert.DeserializeObject(jsonStringBus160.Result);
+                dynamic jsonResponseBus378g = JsonConvert.DeserializeObject(jsonStringBus378g.Result);
+                dynamic jsonResponseBus378j = JsonConvert.DeserializeObject(jsonStringBus378j.Result);
+
+                rer_list.ItemsSource = jsonResponseRERA.response.schedules;
+                bus157_list.ItemsSource = jsonResponseBus157.response.schedules;
+                bus160_list.ItemsSource = jsonResponseBus160.response.schedules;
+                bus378g_list.ItemsSource = jsonResponseBus378g.response.schedules;
+                bus378j_list.ItemsSource = jsonResponseBus378j.response.schedules;
             }
             else
             {
-                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+                MessageBox.Show("Error Code" + respRERA.StatusCode + " : Message - " + respRERA.ReasonPhrase);
+                MessageBox.Show("Error Code" + respBus157.StatusCode + " : Message - " + respBus157.ReasonPhrase);
+                MessageBox.Show("Error Code" + respBus160.StatusCode + " : Message - " + respBus160.ReasonPhrase);
+                MessageBox.Show("Error Code" + respBus378g.StatusCode + " : Message - " + respBus378g.ReasonPhrase);
+                MessageBox.Show("Error Code" + respBus378j.StatusCode + " : Message - " + respBus378j.ReasonPhrase);
             }
 
         }
